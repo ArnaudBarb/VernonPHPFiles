edd4556504c583ce7a73e284163900107465a42d
Arnaud Barberon <barberon_a@hotmail.fr>
2022-02-14 12:13:25 +0100

    Fonction file
    renvoie un tableau indicé;
    
    Fonction nl2br

--- index.php
+++ index.php
@@ -5,3 +5,26 @@ $fichier1 = file_get_contents('./files/corbeau.txt');
 echo "<pre>";
 echo $fichier1;
 echo "</pre>";
+
+echo "<hr />";
+
+$fichier1 = file('./files/corbeau.txt');
+var_dump($fichier1);
+
+echo "<hr />";
+
+$html = "<blockquote>";
+
+    for ($i = 0; $i < count($fichier1); $i++){
+        $html .= $fichier1[$i];
+        $html .= "<br />";
+}
+
+$html .= "</blockquote>";
+
+echo $html;
+
+echo "<hr />";
+
+//Affiche le fichier avecmise en forme d'origine (retours chariots remplacés par <br />)
+echo nl2br(file_get_contents('./files/corbeau.txt'));
